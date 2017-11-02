39a6df9303739034f4abdaa1b0dd067ae1fca2c6
Peterlau7520 <peterlau7520@gmail.com>
2017-11-02 11:31:17 +0800

    Fixing bugs

--- server.js
+++ server.js
@@ -85,8 +85,8 @@ app.post('/register', (req, res) => {
           user = new Resident({
             name: req.body.name,
             email: req.body.email,
-            unit: unit,
-            block: block,
+            // unit: unit,
+            // block: block,
             password: req.body.password,
             estateName : estate.estateName
           });
