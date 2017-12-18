c5725c96d44a7bdc77381e66095402a2d0aaf2ec
Peter Lau <peterlau7520@gmail.com>
2017-12-18 16:39:02 +0800

    modified summary

--- models/models.js
+++ models/models.js
@@ -168,6 +168,8 @@ const userAnswersSchema = new Schema({
 const meetingSchema = new Schema({
     title: String,
     titleChn: String,
+    meetingSummary: String,
+    meetingSummaryChn: String,
     startTime: String,
     endTime: String,
     venue: String,
