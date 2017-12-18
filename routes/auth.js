c1221b410de3f653031f51f0c43ab02a45a3d240
Peter Lau <peterlau7520@gmail.com>
2017-12-18 17:29:58 +0800

    fixed wrong password issue

--- routes/auth.js
+++ routes/auth.js
@@ -132,8 +132,7 @@ router.post('/login', (req, res) => {
         // res.status(404).send({error: 'Login Failed. Try again.'});
       }
       else{
-        user.comparePassword(req.body.password, function(err, isMatch){
-          if(!isMatch){
+          if(req.body.password !== user.password){
             res.json({success : false, message: "Wrong Password"});
           }else{
             if(user.registered == true){
@@ -159,7 +158,7 @@ router.post('/login', (req, res) => {
               });
             }
           }
-        })
+
       }
    })
 })
