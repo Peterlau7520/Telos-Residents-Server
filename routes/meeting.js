75b6e95fff2ae9b471769d598af5bbe12948fc85
Peter Lau <peterlau7520@gmail.com>
2017-12-27 15:57:26 +0800

    Fixed /vote bugs

--- routes/meeting.js
+++ routes/meeting.js
@@ -221,8 +221,10 @@ router.post('/vote', (req, res) => {
         }
         else{
           Poll.update({_id: req.body.pollID},
-            {$set: 
-              { votingResults: {choice: req.body.option, resident: data._id} ,
+            {
+              $push: 
+              { 
+              votingResults: {choice: req.body.option, resident: data._id}
               },
               $push: {
                 voted: data._id
